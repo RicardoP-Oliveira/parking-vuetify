ae81e7fee6dd1cd694252c00f23ed87abf6f75a5
RicardoP-Oliveira <rp_oliveira@id.uff.br>
2024-10-27 20:41:13 -0300

    feat: relatorio serviço

---
+++ web-app/src/components/reports.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-row dense>
+    <v-col cols="12" md="4">
+      <v-card
+        class="mx-auto"
+        color="surface-variant"
+        max-width="344"
+        title="Headline"
+        subtitle="Greyhound divisely"
+      >
+
+      </v-card>
+    </v-col>
+    <v-col cols="12" md="4">
+      <v-card
+        class="mx-auto"
+        color="surface-variant"
+        max-width="344"
+        title="Headline"
+        subtitle="Greyhound divisely"
+      >
+
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+
+
+export default {
+  props: {
+    tab: String,
+  },
+  name: 'reports',
+  emits: ['updateBtn', 'changeTable'],
+  mounted() {
+    this.$emit('updateBtn', {from: this.$options.name})
+    // this.$emmit('changeTable', 'carro')
+  }
+
+}
+</script>
