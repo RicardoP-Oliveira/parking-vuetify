472bc77a972e6ba8b0fa4c1d47683391fa3f6689
RicardoP-Oliveira <rp_oliveira@id.uff.br>
2024-10-27 20:37:47 -0300

    feat: função adicionar usuário

--- web-app/src/services/UserServices.js
+++ web-app/src/services/UserServices.js
@@ -10,4 +10,15 @@ export default class UserService {
       }
     }).then((res) => res.json());
   }
+
+  static adicionar(formData, token) {
+    return fetch(caminho, {
+      headers: {
+        'Content-type': 'application/json;charset=UTF-8',
+        Authorization: token,
+      },
+      body: JSON.stringify(formData),
+      method: 'POST',
+    }).then((res) => res.json());
+  }
 }
