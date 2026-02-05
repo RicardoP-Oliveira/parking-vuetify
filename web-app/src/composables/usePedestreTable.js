7a970f08e5531046f92264eefef8bc3e7d89e1c6
Ricardo <rp_oliveira@id.uff.br>
2026-02-05 12:53:53 -0300

    create: composable TablePedestre

---
+++ web-app/src/composables/usePedestreTable.js
@@ -0,0 +1,82 @@
+// scr/composables/usePedestreTable.js
+export function usePedestreTable(pedestreService) {
+    const loadPedestreData = async (
+        page,
+        itemsPerPage,
+        token,
+        tab,
+        filters
+    ) => {
+        const apiFilters = {
+            documento: filters.documento,
+            pedestre: filters.condutor,
+            dataInicio: filters.dataInicio
+                ? filters.dataInicio.toISOString().split('T')[0]
+                : null,
+            dataFim: filters.dataFim
+                ? filters.dataFim.toISOString().split('T')[0]
+                : null,
+            horaInicio: filters.horaInicio,
+            horaFim: filters.horaFim,
+        };
+
+        // remove filtros vazios
+        Object.keys(apiFilters).forEach((key) => {
+            if (apiFilters[key] === null || apiFilter[key] === '') {
+                delete apiFilters[key];
+            }
+        });
+
+        return pedestreService.getTodos(
+            page,
+            itemsPerPage,
+            token,
+            tab,
+            apiFilters
+        )
+    };
+
+    const headerOrder = () => [
+        'tDoc',
+        'nDoc',
+        'name',
+        'destino',
+        'entrada',
+        'saida'
+    ];
+
+    const setColumns = () => [
+        'tDoc',
+        'nDoc',
+        'name',
+        'destino',
+        'entrada',
+        'hEntrada',
+        'saida',
+        'hSaida'
+    ]
+
+    const setHeaderGroups = () => ({
+        entrada: {
+            title: 'Entrada',
+            children: [
+                { key: 'entrada', title: 'Data'},
+                { key: 'hEntrada', title: 'Hora'}
+            ]
+        },
+        saida : {
+            title: 'Saida',
+            children: [
+                { key: 'saida', title: 'Data'},
+                { key: 'hSaida', title: 'Hora'}
+            ]
+        }
+    });
+
+    return {
+        loadPedestreData,
+        headerOrder,
+        setColumns,
+        setHeaderGroups
+    };
+}
