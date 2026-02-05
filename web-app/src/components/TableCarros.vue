f64687d9c8dd8ffd45a6ee2d8f7965e687ab6152
Ricardo <rp_oliveira@id.uff.br>
2026-02-05 16:22:52 -0300

    create: composable carros

--- web-app/src/components/TableCarros.vue
+++ web-app/src/components/TableCarros.vue
@@ -1,4 +1,4 @@
-<template>
+<!--<template>
   <BaseTable
     :data-service="loadCarData"
     :get-header-order="headerOrder"
@@ -93,6 +93,55 @@ export default {
       };
       return headerGroups;
     },
+  },
+};
+</script>-->
+
+<template>
+  <BaseTable
+    :data-service="loadCarData"
+    :get-header-order="headerOrder"
+    :get-columns="setColumns"
+    :get-header-groups="setHeaderGroups"
+    :filters="filters"
+    :tab="tab"
+    @update-btn="$emit('update-btn', $event)"
+    @changeTable="$emit('changeTable', $event)"
+    ref="baseTableRef"
+  />
+</template>
+
+<script>
+import { getCurrentInstance } from 'vue';
+import BaseTable from '@/components/BaseTable.vue';
+import { useCarroTable } from '@/composables/useCarroTable';
+
+export default {
+  name: 'TableCarros',
+  components: {
+    BaseTable,
+  },
+  props: {
+    tab: String,
+    filters: Object,
+  },
+  emits: ['update-btn', 'changeTable'],
+  setup() {
+    const { proxy } = getCurrentInstance();
+
+    const {
+      loadCarData,
+      headerOrder,
+      setColumns,
+      setHeaderGroups,
+    } = useCarroTable(proxy.$ceicsservice);
+
+    return {
+      loadCarData,
+      headerOrder,
+      setColumns,
+      setHeaderGroups,
+    };
   },
 };
 </script>
---
+++ web-app/src/composables/useCarroTable.js
@@ -0,0 +1,88 @@
+// scr/composables/useCarroTable.js
+export function useCarroTable(carroService) {
+  const loadCarData = async (
+    page,
+    itemsPerPage,
+    token,
+    tab,
+    filters
+  ) => {
+    const apiFilters = {
+      documento: filters.documento,
+      pedestre: filters.condutor,
+      dataInicio: filters.dataInicio
+          ? filters.dataInicio.toISOString().split('T')[0]
+          : null,
+      dataFim: filters.dataFim
+          ? filters.dataFim.toISOString().split('T')[0]
+          : null,
+      horaInicio: filters.horaInicio,
+      horaFim: filters.horaFim,
+    };
+
+    // remove filtros vazios
+    Object.keys(apiFilters).forEach((key) => {
+      if (apiFilters[key] === null || apiFilters[key] === '') {
+          delete apiFilters[key];
+      }
+    });
+
+    return carroService.getTodos(
+      page,
+      itemsPerPage,
+      token,
+      tab,
+      apiFilters
+    )
+  };
+
+    const headerOrder = () => [
+      'placa',
+      'marcaModelo',
+      'entrada',
+      'destino',
+      'saida',    
+    ];
+
+    const setColumns = () => [
+        'placa',
+        'marcaModelo',
+        'entrada',
+        'hEntrada',
+        'eRg',
+        'eCondutor',
+        'saida',
+        'hSaida',
+        'sRg',
+        'sCondutor',
+        'destino',
+    ]
+
+    const setHeaderGroups = () => ({
+         entrada: {
+      title: 'Entrada',
+      children: [
+        { key: 'entrada', title: 'Data' },
+        { key: 'hEntrada', title: 'Hora' },
+        { key: 'eRg', title: 'Documento' },
+        { key: 'eCondutor', title: 'Condutor' },
+      ],
+    },
+    saida: {
+      title: 'Saída',
+      children: [
+        { key: 'saida', title: 'Data' },
+        { key: 'hSaida', title: 'Hora' },
+        { key: 'sRg', title: 'Documento' },
+        { key: 'sCondutor', title: 'Condutor' },
+      ],
+    },
+  });
+
+    return {
+        loadCarData,
+        headerOrder,
+        setColumns,
+        setHeaderGroups
+    };
+}
